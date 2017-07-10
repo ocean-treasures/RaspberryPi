a0c6306a81a9e2040d41769ee9e0dd45569b7203
Willuminat <velislavvelikov2@gmail.com>
2017-07-10 11:48:29 +0300

    Update pipi.py

--- ocean_motion/pipi.py
+++ ocean_motion/pipi.py
@@ -9,33 +9,34 @@ and counter clockwise with negative value of distance
 
 """
 
-def move(distance):
-	"""
-	Distance - number, cm
-	Move the motor up
-	Additional weight makes easier or harder motor's movement
-	Needs calibration
-	
-	"""
-	if distance > 0:
-		speed = 480
-	elif distance < 0:
-		speed = -480
-		distance = -distance
-	else:
-		print("Not moving!")
-	duration = float(distance*0.112)
-	print(duration)
-	motors.motor2.setSpeed(speed);
-	time.sleep(duration);
-	motors.motor2.setSpeed(0);
-	pass
+if __name__ == '__main__':
+	def move(distance):
+		"""
+		Distance - number, cm
+		Move the motor up
+		Additional weight makes easier or harder motor's movement
+		Needs calibration
+
+		"""
+		if distance > 0:
+			speed = 480
+		elif distance < 0:
+			speed = -480
+			distance = -distance
+		else:
+			print("Not moving!")
+		duration = float(distance*0.112)
+		print(duration)
+		motors.motor2.setSpeed(speed);
+		time.sleep(duration);
+		motors.motor2.setSpeed(0);
+		pass
 
 
-def check_step(duration, speed):
-	# duration = 1 # seconds
-	# speed = 480 # MAX_SPEED
-	# step = 9 # cm
-	motors.motor2.setSpeed(speed);
-	time.sleep(duration);
-	motors.motor2.setSpeed(0);
+	def check_step(duration, speed):
+		# duration = 1 # seconds
+		# speed = 480 # MAX_SPEED
+		# step = 9 # cm
+		motors.motor2.setSpeed(speed);
+		time.sleep(duration);
+		motors.motor2.setSpeed(0);
