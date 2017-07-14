68e9ab169cc458d84044a5c127adbb04a9ef018d
Willuminat <velislavvelikov2@gmail.com>
2017-07-14 11:21:30 +0300

    Update motion.py

--- motion.py
+++ motion.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from pololu_drv8835_rpi import motors, MAX_SPEED
 from multiprocessing import Process 
 import time
+
 """
 Moving 2nd motor of pololu_drv8835_rpi driver
 Positive value means clockwise rotation
@@ -15,6 +16,7 @@ Distance = 9, cm
 
 """
 
+
 def move_(distance):
 	"""
 		Moving the motor
@@ -23,37 +25,47 @@ def move_(distance):
 		
 	"""
 	movement = False  
-	speed = 0 
+	speed = 250
 
 	if distance > 0:
 		movement = True # Motor will move
-		speed = 480 # 19.2 MHz / 2 / 480 = 20 kHz
+		#speed = 250 # 19.2 MHz / 2 / 480 = 20 kHz
 		# Max speed for a motor with this driver
 		
 	elif distance < 0:
 		movement = True 
-		speed = -480 # Max speed for a motor with this driver for the other direction
+		speed = -speed
+		 # Max speed for a motor with this driver for the other direction
 		distance = -distance # Making distance positive for further calculations
 		
 	else:
 		movement = False # Motor won't move
 
 	if movement == True: 
-		duration = float(distance*0.112) 
-		print("\n\n Moving {} seconds. \n\n".format(duration)) 
+		duration = float(distance) 
+		print("\n Moving {} seconds. \n".format(duration)) 
+		# Prints on screen how much time the motor will rotate
 		motors.motor2.setSpeed(speed); # Starting motor
-		time.sleep(duration); # Time for motor to rotate, seconds
-		motors.motor2.setSpeed(0); # Turning off the motor *both motors in the driver*
+		time.sleep(duration*0.3); # Time for motor to rotate, seconds
+		motors.motor2.setSpeed(0); 
+		# Turning off the motor *both motors in the driver*
+	
 	else:	
 		pass 
 
+
 def move(dist, sync=False):
-	if sync==True:
-    	    dist = float(dist) 
-	    Process(target=move_,args=[dist]).start() # Starts new process to turn the motor
-  else:
-    move_(dist)
+    
+    if sync==True :
+        dist = float(dist) 
+        Process(target=move_,args=[dist]).start() 
+        # Starts new process to turn the motor
+    
+    else :
+        move_(dist)
+
 
 if __name__ == '__main__':
-    dist_ = float(raw_input("Distance in cm: ")) 
-    move(dist_)
+	
+	dist_ = float(raw_input("Distance in cm: ")) 
+	move(dist_)
