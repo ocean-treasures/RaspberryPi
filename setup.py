3d79c1904f8f6394e96968a304ded0e6d080f25d
Willuminat <velislavvelikov2@gmail.com>
2017-09-04 22:12:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
-setup(name='pololu_drv8835_rpi',
-      version='2.0.0',
+setup(name='ocean-motion',
+      version='1.5.0',
       description=('Library for the Pololu DRV8835 Dual Motor'
-                   'Driver Kit for Raspberry Pi Model B+'),
+                   'Driver Kit for Raspberry Pi Model B+, second motor'),
       url='http://www.pololu.com/product/2753',
       py_modules=['pololu_drv8835_rpi'],
       )
